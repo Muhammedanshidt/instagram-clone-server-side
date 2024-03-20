68643c8feb4610727879bc6d86ba74c376964465
Anshid <muhammedanshidh.t@gmail.com>
2024-03-20 11:45:38 +0530

    deploy 11th

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ app.use(express.json())
 app.use(cookies());
 
 
-app.use(cors({
-        origin: "https://instagram-clone-client-side.vercel.app",
-        redentials: true,
-}))
+// app.use(cors({
+//         origin: "https://instagram-clone-client-side.vercel.app",
+//         redentials: true,
+// }))
 // app.use(cors({
 //     origin: "http://localhost:3000", 
 //     credentials: true,
 // }));
 
+app.use(cors())
 
 //user area
 const routeSingUp = require("./Routes/UserRoute");
