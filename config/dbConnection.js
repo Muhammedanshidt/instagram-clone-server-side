bd956541cf85675b9b861e77fc0c33f6cd1da079
Anshid <muhammedanshidh.t@gmail.com>
2024-05-13 15:27:16 +0530

    deploy 47

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,7 +4,7 @@ console.log("hellow we are here")
 
 const dbConnection = () => {
 
-    mongoose.connect("mongodb+srv://muhammedanshidht:Zwcbu2AgCU0PGbrL@cluster0.oozpamy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    mongoose.connect("mongodb+srv://muhammedanshidht:Zwcbu2AgCU0PGbrL@cluster0.oozpamy.mongodb.net/")
         .then(() => console.log("successfully connected to mongodb", mongoose.connection.host)).catch((err) => console.log(err))
 };
 
