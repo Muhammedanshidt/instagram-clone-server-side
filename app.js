442ea68b566623063b23edbf513fada5cc333414
Anshid <muhammedanshidh.t@gmail.com>
2024-05-21 11:12:45 +0530

    63

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require("cors")
+const cors = require('cors');
 const cookies = require("cookie-parser")
 const bodyParser = require('body-parser')
 const app = express();
@@ -14,14 +14,15 @@ app.use(cookies());
 
 app.use(bodyParser.json());
 
-app.use(cors({
-        origin: "https://instagram-clone-client-side.vercel.app",
-        credentials: true,
-}))
-// app.use(cors({
-//     origin: "http://localhost:3000", 
-//     credentials: true,
-// }));
+const corsOptions = {
+        origin: 'https://instagram-clone-client-side.vercel.app',
+        credentials: true, 
+      };
+
+
+app.use(cors(corsOptions))
+
+
 
 
 //user area
